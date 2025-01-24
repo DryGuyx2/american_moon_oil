2eddc8d4668414ceb2fd37eb0fcdcc514e1fb20c
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-01-25 00:38:23 +0100

    Add cursor layer movement, and untrack .venv

--- cursor.py
+++ cursor.py
@@ -4,9 +4,10 @@ class Cursor:
     def __init__(self, grid_size):
         self._position = (0, 0)
         self._grid_size = grid_size
+        self._height = 0
 
     def __repr__(self):
-        return f"{self.position}"
+        return f"{self._height}:{self._position}"
 
     @property
     def position(self):
@@ -15,6 +16,14 @@ class Cursor:
     @position.setter
     def position(self, new_position):
         self._position = new_position
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, new_height):
+        self._height = new_height
 
 def is_touching_edge(position, grid_size, edge):
     if edge == "left":
@@ -28,7 +37,7 @@ def is_touching_edge(position, grid_size, edge):
 
     raise NameError("Invalid edge name")
 
-def move_cursor(event, cursor, grid_size):
+def move_cursor(event, cursor, grid_size, max_height):
     if event.key == pygame.K_LEFT and not is_touching_edge(cursor.position, grid_size, "left"):
         cursor.position = (cursor.position[0] - 1, cursor.position[1])
     elif event.key == pygame.K_RIGHT and not is_touching_edge(cursor.position, grid_size, "right"):
@@ -37,3 +46,9 @@ def move_cursor(event, cursor, grid_size):
         cursor.position = (cursor.position[0], cursor.position[1] - 1)
     elif event.key == pygame.K_DOWN and not is_touching_edge(cursor.position, grid_size, "bottom"):
         cursor.position = (cursor.position[0], cursor.position[1] + 1)
+    elif event.key == pygame.K_RETURN:
+        if cursor.height >= max_height:
+            cursor.height = 0
+            return
+
+        cursor.height += 1
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ for key, image in tile_to_image_map.items():
 # Initialize grid with moon floor tile sprite as bottom layer
 grid = tile.TileGrid(GRID_SIZE)
 tile.fill_grid(grid, "moon_floor")
+grid.grid[0][0].layers.append("oil_pump")
 print(f"Grid:\n{grid}")
 
 tile_cursor = cursor.Cursor(GRID_SIZE)
@@ -35,19 +36,23 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("American Moon Oil")
 
-while True:
-    screen.fill((0, 0, 0))
-    tile.draw_grid(grid, screen, tile_to_image_map, TILE_SIZE)
-
-    pygame.display.flip()
-
+def handle_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            cursor.move_cursor(event, tile_cursor, GRID_SIZE)
+            max_cursor_height = len(grid.grid[tile_cursor.position[0]][tile_cursor.position[1]].layers) - 1
+            cursor.move_cursor(event, tile_cursor, GRID_SIZE, max_cursor_height)
             print(f"Cursor: {tile_cursor}")
+
+while True:
+    handle_events()
+
+    screen.fill((0, 0, 0))
+    tile.draw_grid(grid, screen, tile_to_image_map, TILE_SIZE)
+
+    pygame.display.flip()
 
     clock.tick(FRAMERATE)
